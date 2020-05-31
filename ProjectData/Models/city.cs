f79f6217224d46e7b514a4d0ef2ea6b5fbd61cbb
hunterlan <kotyshe@gmail.com>
2020-05-31 12:54:40 +0300

    Added model and DB

---
+++ ProjectData/Models/city.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectData.Models
+{
+    public class City
+    {
+        public int city_id { get; set; }
+
+        public int country_id { get; set; }
+
+        public int region_id { get; set; }
+
+        public string name { get; set; }
+    }
+}
