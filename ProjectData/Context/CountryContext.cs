34de5dd2c003b0e69d4a8ee6c41ecb26e98d960d
hunterlan <kotyshe@gmail.com>
2020-05-31 22:25:15 +0300

    Some cosmetic changes

--- ProjectData/Context/CountryContext.cs
+++ ProjectData/Context/CountryContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
 using ProjectData.Models;
 
 namespace ProjectData.Context
